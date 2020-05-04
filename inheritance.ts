16ad58df0f8a4abe817eeb8a72ef289ac214a6e8
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-05-04 21:30:56 +0530

    Inherit a class.

---
+++ inheritance.ts
@@ -0,0 +1,25 @@
+class Person {
+    public name: string
+    private age: number
+    protected height: string
+    constructor(name: string, age: number, height: string) {
+        this.name = name
+        this.age = age
+        this.height = height
+    }
+}
+
+class Student extends Person {
+    id: number
+    constructor(name: string, age: number, height: string, id: number) {
+        super(name, age, height)
+        this.id = id
+    }
+}
+
+const person1 = new Person('John', 27, '5')
+console.log(person1);
+
+const student1 = new Student('Jane', 24, '4.5', 51)
+console.log(student1);
+
