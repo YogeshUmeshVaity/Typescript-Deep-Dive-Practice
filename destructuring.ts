dc2e16a84df523875a8c918b2afd3014451924a0
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-05-10 00:20:25 +0530

    Destructure the deep data in a structure.

--- destructuring.ts
+++ destructuring.ts
@@ -8,8 +8,14 @@ console.log(height, destination, color);
 // Change value
 mountain.height = 16000;
 
-// Update object. Reuse the existing variables.
+// Update object. Reuse the existing variables
 ({height, destination, color} = mountain)
 
 console.log(height, destination, color);
 
+// Deep data
+const house = { coffeeMachine: { size: 24 } }
+// Destructuring of the deep data
+const {coffeeMachine: {size}} = house
+console.log(size);
+console.log(house.coffeeMachine.size === size)
