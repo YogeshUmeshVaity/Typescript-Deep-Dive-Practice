80da43f04df42da4fe79a37fd57d7a6308eed2d4
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-06-30 21:50:58 +0530

    Use export { } block to export modules.

--- module-for-export.ts
+++ module-for-export.ts
@@ -1,1 +1,10 @@
 export const airPurifiers = ['Snake Plant', 'Parlour Palm', 'Rubber Plant']
+
+const humidifiers = ['Areca Palm', 'Boston Fern', 'Spider Plant']
+
+const detoxifiers = ['Peace Lily', 'Chinese Evergreen', 'Bamboo Plant']
+
+export {
+    humidifiers,
+    detoxifiers
+}
--- module-test.ts
+++ module-test.ts
@@ -1,6 +1,12 @@
 
 import { airPurifiers as purifiers, airPurifiers } from "./module-for-export"
+import { humidifiers, detoxifiers } from "./module-for-export"
 
 // Both allowed
 console.log(purifiers);     // use alias
 console.log(airPurifiers)   // use actual name
+
+// exported using export { } block
+console.log(humidifiers)
+console.log(detoxifiers)
+
