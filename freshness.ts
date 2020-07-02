98c7bafc121b2e5a2af4b497202973aa25c8d14c
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2020-07-02 16:03:46 +0530

    Use Freshness(strict object literal checking) to avoid typo or misunderstanding of the API.

---
+++ freshness.ts
@@ -0,0 +1,15 @@
+/**
+ * The concept of Freshness is also called 'Strict Object Literal Checking'. The reason why only
+ * object literals are type checked this way is because in this case additional properties that
+ * aren't actually used is almost always a typo or a misunderstanding of the API.
+ */
+function logName(something: { name: string }) {
+    console.log(something.name)
+}
+
+const animal = { name: 'Cat', color: 'Golden' }
+const person = { name: 'Sandy', skill: 'DOP' }
+
+logName(animal)
+logName(person)
+// logName({name: 'Crow', color: 'Manhattan'})       // Error when a literal is passed
